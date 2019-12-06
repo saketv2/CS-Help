ec9aaa5c31ed90e35b69a09d819580fcb27d22dc
saketv2 <saketv2@illinois.edu>
2019-12-06 17:23:32 -0600

    Database testing

--- app/src/main/java/com/example/cshelp/MainActivity.java
+++ app/src/main/java/com/example/cshelp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.cshelp;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -8,16 +9,27 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+
 public class MainActivity extends AppCompatActivity {
 
     boolean USER_CHECKED_IN;
-
+    private DatabaseReference database;
+    private Integer count;
     Button checkInButton;
     Button checkOutButton;
     ImageButton refreshButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        database = FirebaseDatabase.getInstance().getReference();
+        database.child("count").setValue(count);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -77,6 +89,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void checkIn() {
+        database.child("count").setValue(count++);
         USER_CHECKED_IN = true;
         Log.i("checkIn Button", "The button for checking in was clicked.");
         // web request to server
@@ -84,6 +97,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void checkOut() {
+        database.child("count").setValue(count++);
         USER_CHECKED_IN = false;
         Log.i("checkOut Button", "The button for checking out was clicked.");
         // web request to server
@@ -105,7 +119,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public int getLiveCount() {
-        // will get from server
         return 0;
     }
 
